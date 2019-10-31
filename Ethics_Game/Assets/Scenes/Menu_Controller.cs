0a0d4a60c2d68226a53d2d3454d147dbe0ad328b
SAI RAMA KRISHNA TUMMALA <34177350+sairamktummala@users.noreply.github.com>
2019-10-31 14:55:11 -0500

    adding level select screen and mini game

---
+++ Ethics_Game/Assets/Scenes/Menu_Controller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu_Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartGame () {
+
+		SceneManager.LoadScene (1);
+	}
+
+    public void StartGame_Easy()
+    {
+
+    }
+
+    public void StartGame_Medium()
+    {
+
+        SceneManager.LoadScene(3);
+    }
+
+    public void StartGame_Hard()
+    {
+
+    }
+
+    public void Rematch()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void QuitGame () {
+
+		Application.Quit ();
+	}
+
+}
