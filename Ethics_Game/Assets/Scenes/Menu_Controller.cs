6908bd19a8feb6e0b3fb3482edccb51fb84f22c1
Daniel Bowen <Daniel.Bowen@guideit.com>
2019-10-31 16:39:49 -0500

    Removed unnecessary functions from Menu_Controller

--- Ethics_Game/Assets/Scenes/Menu_Controller.cs
+++ Ethics_Game/Assets/Scenes/Menu_Controller.cs
@@ -6,17 +6,7 @@ using UnityEngine;
 
 public class Menu_Controller : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	public void StartGame () {
+    public void StartGame () {
 
 		SceneManager.LoadScene (1);
 	}
