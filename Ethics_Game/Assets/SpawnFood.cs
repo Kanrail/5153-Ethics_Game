613c0e5c73a74d8a7b2cdb535b839dfe44b10398
Daniel Bowen <Daniel.Bowen@guideit.com>
2019-11-02 15:44:51 -0500

    Worked with Ben to get mostly working Snake game up and running, still need to add lose condition and score counter as well as clean up code.

---
+++ Ethics_Game/Assets/SpawnFood.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnFood : MonoBehaviour
+{
+    // Food Pellet Object
+    public GameObject foodPrefab;
+
+    // Border Transformation Variables
+    public Transform borderTop;
+    public Transform borderBottom;
+    public Transform borderLeft;
+    public Transform borderRight;
+
+    //Function Name: Start
+    //Parameters: None
+    //Description: Default Initializer, handles the beginning of the food spawning function
+    void Start()
+    {
+        // Spawn food every 4 seconds, starting in 3
+        InvokeRepeating("Spawn", 3, 4);
+    }
+
+    //Function Name: Spawn
+    //Parameters: None
+    //Description: Randomly places a food pellet in between the established borders
+    void Spawn()
+    {
+        // x position between left & right border
+        int x = (int)Random.Range(borderLeft.position.x,
+            borderRight.position.x);
+
+        // y position between top & bottom border
+        int y = (int)Random.Range(borderBottom.position.y,
+            borderTop.position.y);
+
+        // Instantiate the food at (x, y)
+        Instantiate(foodPrefab,
+            new Vector2(x, y),
+            Quaternion.identity); // default rotation
+    }
+}
