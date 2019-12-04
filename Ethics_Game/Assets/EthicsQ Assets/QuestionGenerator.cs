629aae0f101d8686b90138be2677303675272a41
Daniel Bowen <Daniel.Bowen@guideit.com>
2019-12-03 18:35:11 -0600

    Just in case the beginning of the ethics question decides to delete itself, going to do periodic commits to make sure nothing is lost

---
+++ Ethics_Game/Assets/EthicsQ Assets/QuestionGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class QuestionGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string path = "Assets/EthicsQ Assets/Questions_Easy.txt";
+
+        Dictionary<string,List<string> > questionDict = new Dictionary<string, List<string>>();
+        //StreamReader read = new StreamReader(path, true);
+        string[] lines = System.IO.File.ReadAllLines(path);
+
+        string question="";
+
+        List<string> answerList = new List<string>();
+
+        foreach (string line in lines)
+        {
+            char[] tempLineList = line.ToCharArray();
+            string lineMinusDelim = tempLineList.Skip(1).ToString();
+            if (tempLineList[0]=='*')
+            {
+                question = lineMinusDelim;
+                questionDict.Add(question,answerList);
+            }
+            else if (tempLineList[0] == '-')
+            {
+                questionDict[question].Add(lineMinusDelim);
+            }
+            if (tempLineList[0] == '$')
+            {
+                questionDict[question].Add(lineMinusDelim);
+            }
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
