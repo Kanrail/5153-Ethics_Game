079b2078ebc02234edd5949649325941a95acc16
SAI RAMA KRISHNA TUMMALA <34177350+sairamktummala@users.noreply.github.com>
2019-12-04 21:20:58 -0600

    wireframing completed

---
+++ Ethics_Game-finalversion/Assets/EthicsQ Assets/EthicsQuestionHandler.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using Random = UnityEngine.Random;
+
+public class EthicsQuestionHandler : MonoBehaviour
+{
+    private Dictionary<string, List<string>> questionDict;
+
+    public Text QuestionTextLabel;
+
+    public Text Answer1Label;
+
+    public Text Answer2Label;
+
+    public Text Answer3Label;
+
+    public Text Answer4Label;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string path = "Assets/EthicsQ Assets/Questions_Easy.txt";
+
+        questionDict = new Dictionary<string, List<string>>();
+        //StreamReader read = new StreamReader(path, true);
+        string[] lines = System.IO.File.ReadAllLines(path);
+
+        string question="";
+
+        List<string> answerList = new List<string>();
+
+        foreach (string line in lines)
+        {
+            char[] tempLineList = line.ToCharArray();
+            string lineMinusDelim = tempLineList.Skip(1).ToString();
+            if (tempLineList[0]=='*')
+            {
+                question = lineMinusDelim;
+                questionDict.Add(question,answerList);
+            }
+            else if (tempLineList[0] == '-')
+            {
+                questionDict[question].Add(lineMinusDelim);
+            }
+            if (tempLineList[0] == '$')
+            {
+                questionDict[question].Add(lineMinusDelim);
+            }
+
+        }
+
+    }
+
+    private string GetRandomQuestion()
+    {
+        int randomNum = Random.Range(1,questionDict.Count);
+        return questionDict.ElementAt(randomNum).Key;
+    }
+
+    public void SetAnswerButtons(string question)
+    {
+        QuestionTextLabel.text = question;
+        Answer1Label.text = questionDict[question][0];
+        Answer2Label.text = questionDict[question][1];
+        Answer3Label.text = questionDict[question][2];
+        Answer4Label.text = questionDict[question][3];
+    }
+
+    private bool CheckSelectedAnswer(string selectedAnswer, string question)
+    {
+        if (questionDict[question][5] != selectedAnswer)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private void WinningAnswer()
+    {
+
+    }
+
+    private void LosingAnswer()
+    {
+
+    }
+
+    public void AnswerButtonClick(Button button)
+    {
+        string selectedAnswer = button.name;
+        string question = QuestionTextLabel.text;
+        bool correctAnswer = CheckSelectedAnswer(selectedAnswer, question);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Ethics_Game-finalversion/Assets/MainMenu&LSelect_Assets/Menu_Controller.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu_Controller : MonoBehaviour {
+
+    public void Start()
+    {
+        PlaySoundFile();
+
+        Debug.Log("Start");
+    }
+
+    public void StartGame () {
+
+		SceneManager.LoadScene ("Level Select Screen");
+	}
+
+    public void StartGame_Easy()
+    {
+        SceneManager.LoadScene("Game2");
+    }
+
+    public void StartGame_Medium()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void StartGame_Hard()
+    {
+        SceneManager.LoadScene("FlappyBird");
+    }
+
+    public void PongStartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void GoMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void SnakeGameRestart()
+    {
+        SceneManager.LoadScene("Game2");
+    }
+
+    public void LoadEthicsQuestion()
+    {
+        SceneManager.LoadScene("EthicsQuestion");
+    }
+
+    public void Rematch()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    
+    public void PlaySoundFile()
+    {
+        string sceneName = SceneManager.GetActiveScene().name.ToString();
+
+        Debug.Log(sceneName.ToString());
+
+        switch (sceneName)
+        {
+        case "Menu":
+                AudioController.instance.PlayMenuMusic();
+            break;
+        case "Game":
+                AudioController.instance.PlayPongGameMusic();
+                break;
+        case "Game2":
+                AudioController.instance.PlaySnakeGameMusic();
+                break;
+        case "FlappyBird":
+                AudioController.instance.PlayFlappyGameMusic();
+                break;
+        }
+    }
+
+}
